ca467e7a08d1b79525538b9726bec77d368e3d2c
kiko33 <79004804+kiko33-E-Com@users.noreply.github.com>
2021-12-30 21:37:11 +0100

    Add files via upload

---
+++ src/main/kotlin/com/behya/Application.kt
@@ -0,0 +1,12 @@
+package com.behya
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.behya.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSecurity()
+        configureRouting()
+    }.start(wait = true)
+}
